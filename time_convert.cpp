dcc030c1d383c8541d8845ffb9c9305b2139e452
diyakhandhediya1607-code <diyakhandhediya1607@gmail.com>
2026-01-08 21:05:14 +0530

    converter
    Added input prompts for hours, minutes, and seconds, and calculated total seconds.

--- time_convert.cpp
+++ time_convert.cpp
@@ -11,5 +11,20 @@ int main(){
     h = sec/3600;
     m = (sec % 3600)/60;
     s = sec%60;
-    cout<<h<<":"<<m<<":"<<s;
+    cout<<h<<":"<<m<<":"<<s<<endl;
+
+    
+    cout<<"enter hour:";
+    cin>>h;
+
+   
+    cout<<"enter minute:";
+    cin>>m;
+
+  
+    cout<<"enter second:";
+    cin>>s;
+ 
+    cout <<"tota second"<<(h*3600+m*60+s)<<endl;
+
 }
